8c55e5bb558162f2adfc8aac9fcfbe6c172586fc
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-08-27 09:29:19 -0400

    Atender siguiente Ticket

--- models/ticket-control.js
+++ models/ticket-control.js
@@ -1,6 +1,12 @@
 const path = require('path');
 const fs = require('fs');
 
+class Ticket {
+    constructor(number, box) {
+        this.number = number;
+        this.box = box;
+    }
+}
 
 class TicketControl {
     constructor() {
@@ -33,6 +39,27 @@ class TicketControl {
     saveDB() {
         const dbPath = path.join(__dirname, '../db/data.json');
         fs.writeFileSync(dbPath, JSON.stringify(this.toJson));
+    }
+    next() {
+        this.latest += 1;
+        const ticket = new Ticket(this.latest, null);
+        this.tickets.push(ticket);
+        this.saveDB();
+        return 'Ticket' + ticket.number;
+    }
+    attendNext(box) {
+        if (this.tickets.length === 0) {
+            return null;
+        }
+        const ticket = this.tickets.shift();
+        ticket.box = box;
+        this.latestFour.unshift(ticket);
+
+        if (this.latestFour.length > 4) {
+            this.latestFour.splice(-1, 1);
+        }
+        this.saveDB();
+        return ticket;
     }
 }
 module.exports = TicketControl;
