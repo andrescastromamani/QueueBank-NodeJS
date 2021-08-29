e672bd0b8a8d01b12f034fc8706215a0bebf4ecb
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-08-28 20:54:32 -0400

    Configuramos la pantalla del escritorio

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,28 @@
+const lblDesktop = document.querySelector('h1');
+const btnAttention = document.querySelector('button');
+const searchParams = new URLSearchParams(window.location.search);
 
+if (!searchParams.has('escritorio')) {
+    window.location = 'index.html';
+    throw new Error('Desktop is required');
+}
+const desktop = searchParams.get('escritorio');
+lblDesktop.innerText = desktop;
 
-console.log('Escritorio HTML');
+const socket = io();
+
+socket.on('connect', () => {
+    btnAttention.disabled = false;
+
+});
+
+socket.on('disconnect', () => {
+    btnAttention.disabled = true;
+});
+socket.on('latest-ticket', (latest) => {
+
+})
+
+btnAttention.addEventListener('click', () => {
+
+});
