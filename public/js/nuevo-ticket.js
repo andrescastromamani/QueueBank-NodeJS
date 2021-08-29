31641ef193f213887e87a0e1a0f5d42b2361a516
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-08-28 20:37:26 -0400

    Generamos un nuevo ticket

--- public/js/nuevo-ticket.js
+++ public/js/nuevo-ticket.js
@@ -1,3 +1,23 @@
+const lblNuevoTicket = document.querySelector('#lblNuevoTicket');
+const button = document.querySelector('button');
 
+const socket = io();
 
-console.log('Nuevo Ticket HTML');
+socket.on('connect', () => {
+    button.disabled = false;
+
+});
+
+socket.on('disconnect', () => {
+    button.disabled = true;
+});
+socket.on('latest-ticket', (latest) => {
+    lblNuevoTicket.innerText = 'Ticket' + latest;
+})
+
+button.addEventListener('click', () => {
+    socket.emit('next-ticket', null, (ticket) => {
+        lblNuevoTicket.innerText = ticket;
+    });
+
+});
--- sockets/controller.js
+++ sockets/controller.js
@@ -3,10 +3,10 @@ const TicketControl = require("../models/ticket-control");
 const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
-    socket.on('emit-message', (payload) => {
-        console.log('Message: ', payload);
-        //Send Payload to Clients
-        socket.broadcast.emit('emit-message', payload);
+    socket.emit('latest-ticket', ticketControl.latest);
+    socket.on('next-ticket', (payload, callback) => {
+        const next = ticketControl.next();
+        callback(next);
     });
 }
 
